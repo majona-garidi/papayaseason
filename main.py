427cf2ee60c741a0e11aaf26d293d273688c4df9
probably-human <106025770+probably-human@users.noreply.github.com>
2023-03-11 11:00:36 +0100

    Implement functionality to return the main season list from the selected country

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout, \
+    QScrollArea
+from PyQt5.QtGui import QPixmap, QMovie, QPalette, QImage
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
-
 import json
-
 from datetime import date
 
 
@@ -14,7 +13,9 @@ widgets = {
     "logo": [],
     "button": [],
     "question": [],
-    "answers": []
+    "answers": [],
+    "result": [],
+    "animation": []
 
 }
 
@@ -22,11 +23,17 @@ app = QApplication(sys.argv)
 
 window = QWidget()
 window.setWindowTitle("Papaya Season")
-window.setFixedWidth(1000)
-# window.move(2700, 200)
+# several buttons are created (downwards the GUI) based on how many countries
+# there are in the json data
+# therefore adjustSize() is used to react to the varying quantity of buttons
+window.adjustSize()
+#window.setFixedWidth(1000)
+#window.move(2700, 200)
 window.setStyleSheet("background: 'white'")
 
 grid = QGridLayout()
+
+clickedAnswerButtonCountry = None
 
 
 def clear_widgets():
@@ -88,6 +95,7 @@ def create_buttons(answer, l_margin = 85, r_margin = 85):
     button = QPushButton(answer)
     button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
     # button.setFixedWidth(485)
+
     button.setStyleSheet(
         "*{border: 4px solid '#353535';" +
         "margin-left: " + str(l_margin) + "px;" +
@@ -100,6 +108,7 @@ def create_buttons(answer, l_margin = 85, r_margin = 85):
         "margin-top: 20px}" +
         "*:hover{background-color: '#C6FF33'}"
     )
+
     return button
 
 
@@ -116,6 +125,7 @@ def frame_1():
     # button widget
     button = QPushButton("START")
     button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+
     button.setStyleSheet(
         "*{border: 4px solid '#353535';" +
         "border-radius: 45px;" +
@@ -125,6 +135,7 @@ def frame_1():
         "margin: 100px 100px;}" +
         "*:hover{background: '#C6FF33';}"
     )
+
     button.clicked.connect(start_game)
 
     widgets["button"].append(button)
@@ -137,13 +148,14 @@ def frame_2():
 
     question = QLabel("Where is your papaya from?")
     question.setAlignment(QtCore.Qt.AlignCenter)
-    question.setWordWrap(True)
+    #question.setWordWrap(True)
     question.setStyleSheet(
         "font-family: 'Helvetica';" +
         "font-size: 25px;" +
         "color: '#353535';" +
         "padding: 75px;"
         "margin: 50px;")
+
     widgets["question"].append(question)
 
     fill_button_with_data()
@@ -163,9 +175,6 @@ def frame_2():
     grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
     # number of rows is dependent on number of countries for flexible position on grid
     grid.addWidget(widgets["logo"][-1], numberOfCountries + 2, 0, 1, 2)
-
-
-clickedAnswerButtonCountry = None
 
 
 def which_button_was_clicked(clickedbutton):
@@ -195,6 +204,21 @@ def fill_button_with_data():
 
 def frame_3():
 
+    season_check()
+
+    result = QLabel("It's " "? " + "in " + clickedAnswerButtonCountry)
+    result.setAlignment(QtCore.Qt.AlignCenter)
+    result.setWordWrap(True)
+    result.setStyleSheet(
+        "font-family: 'Helvetica';" +
+        "font-size: 25px;" +
+        "color: '#353535';"
+        "padding: 75px;"
+        "margin: 50px;"
+    )
+    widgets["result"].append(result)
+    grid.addWidget(widgets["result"][-1], 4, 0, 1, 2)
+
     image = QPixmap("logo_bottom_128px.png")
     logo = QLabel()
     logo.setPixmap(image)
@@ -203,29 +227,36 @@ def frame_3():
     widgets["logo"].append(logo)
     grid.addWidget(widgets["logo"][-1], numberOfCountries + 2, 0, 1, 2)
 
-    print(clickedAnswerButtonCountry)
+    get_main_season_list_from_clicked_button()
+
+    get_availability_list_from_clicked_button()
 
 
-def get_main_season_list_from_json():
+def get_main_season_list_from_clicked_button():
 
     with open("seasons.json", "r") as f:
         data = json.load(f)
 
-        mainSeasons = []
-        # length of list countries in json data for flexible number of countries
-        for i in range(len(data["countries"])):
-            mainSeasons.append(data["countries"][i]["mainSeason"])
-
-        print(mainSeasons)
-
-        return mainSeasons
+    for country_dict in data["countries"]:
+        if country_dict["country"] == clickedAnswerButtonCountry:
+            return country_dict["mainSeason"]
 
 
-def season_checker():
+def get_availability_list_from_clicked_button():
+
+    with open("seasons.json", "r") as f:
+        data = json.load(f)
+
+    for country_dict in data["countries"]:
+        if country_dict["country"] == clickedAnswerButtonCountry:
+            return country_dict["availability"]
+
+
+def season_check():
 
     currentmonth = date.today().month
 
-    seasonlist = get_main_season_list_from_json()[0]
+    seasonlist = get_main_season_list_from_clicked_button()
 
     if currentmonth in seasonlist:
 
@@ -236,10 +267,29 @@ def season_checker():
         print("no")
 
 
+def place_warning_animation_on_grid():
+
+    label = QLabel()
+    animation = QMovie("error_solid_300px.gif")
+    label.setMovie(animation)
+    animation.start()
+    widgets["animation"].append(animation)
+    label.setAlignment(QtCore.Qt.AlignCenter)
+    grid.addWidget(label, 1, 0, 3, 2)
+
+
+def place_affirming_animation_on_grid():
+
+        label = QLabel()
+        animation = QMovie("confetti_solid_300px.gif")
+        label.setMovie(animation)
+        animation.start()
+        widgets["animation"].append(animation)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        grid.addWidget(label, 1, 0, 3, 2)
+
+
 frame_1()
-
-
-season_checker()
 
 
 window.setLayout(grid)
